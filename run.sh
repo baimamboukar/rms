d76c42f5f8b9cd0c039aa01854ca32d56cb1a8fe
baimamboukar <baimamboukar@gmail.com>
2021-12-29 19:43:57 +0100

    runner file

---
+++ run.sh
@@ -0,0 +1,6 @@
+javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/models/*.java
+javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/services/interfaces/*.java
+javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/services/impl/*.java
+javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/*.java
+javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/database/*.java
+cd com/baimamboukar/java/rms/class
