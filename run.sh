0caa696f76537f195c46334248c20b8ee10205d1
baimamboukar <baimamboukar@gmail.com>
2022-01-02 11:24:41 +0100

    done with run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
-javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/models/*.java
-javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/services/interfaces/*.java
-javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/services/impl/*.java
-javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/*.java
-javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/database/*.java
-cd com/baimamboukar/java/rms/class
+# javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/models/*.java
+# javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/services/interfaces/*.java
+# javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/services/impl/*.java
+# javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/*.java
+# javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/database/*.java
+
+# jar cvfm baimamboukar.jar MANIFEST.MF class/* rms.sqlite
+cd com/baimamboukar/java/rms
+#javac -cp ./:jars/* -d class/ src/ui/* src/database/* src/models/*
+java -cp jars/* com.baimamboukar.java.rms.src.ui.Dashboard
