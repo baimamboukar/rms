bc208ae546d6eda769c60db2a5fc0ef14e2dcc7b
baimamboukar <baimamboukar@gmail.com>
2022-01-15 10:38:40 +0100

    to commit class

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
-# cd com/baimamboukar/java/rms
+ cd com/baimamboukar/java/rms/class
 # ls
 for file in * 
     do
             git add $file
-            git commit -m "added $file"
-            git push
+            git commit -m "compiling $file"
+            #git push
     done
