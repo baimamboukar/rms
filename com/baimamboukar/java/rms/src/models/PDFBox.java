b6d8583997cf543c3868633f6aafb6abe6dd9f9a
baimamboukar <baimamboukar@gmail.com>
2022-01-15 14:53:35 +0100

    editing pdf box

--- com/baimamboukar/java/rms/src/models/PDFBox.java
+++ com/baimamboukar/java/rms/src/models/PDFBox.java
@@ -3,18 +3,14 @@ package com.baimamboukar.java.rms.src.models;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Stream;
-import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Paragraph;
@@ -52,17 +48,6 @@ public class PDFBox {
 
             PdfPTable table = new PdfPTable(3);
             addTableHeader(table);
-            addRows(table);
-            try {
-                addCustomRows(table);
-            } catch (URISyntaxException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-
-            document.add(table);
-            document.close();
 
             List<List<String>> records = new ArrayList<>();
 
@@ -80,12 +65,12 @@ public class PDFBox {
                 records.forEach((record) -> {
                     Paragraph paragraph = new Paragraph("", textFont);
                     try {
-
                         Font normalFont = FontFactory.getFont(FontFactory.HELVETICA, 14, BaseColor.DARK_GRAY);
 
                         record.forEach((String cell) -> {
                             Chunk part = new Chunk(cell);
                             paragraph.add(part);
+                            addRows(table, paragraph);
                         });
                         paragraph.setFont(normalFont);
                         paragraph.setExtraParagraphSpace(12);
@@ -99,6 +84,7 @@ public class PDFBox {
             } catch (Exception e) {
                 System.out.println("Not found");
             }
+            document.add(table);
             document.close();
 
         } catch (DocumentException e) {
@@ -109,39 +95,18 @@ public class PDFBox {
     }
 
     private void addTableHeader(PdfPTable table) {
-        Stream.of("MATRICULE", "GRADE")
+        Stream.of("📌 MATRICULE", "📊 GRADE")
                 .forEach(columnTitle -> {
                     PdfPCell header = new PdfPCell();
                     header.setBackgroundColor(BaseColor.GREEN);
-                    header.setBorderWidth(3);
+                    header.setBorderWidth(2);
                     header.setPhrase(new Phrase(columnTitle));
                     table.addCell(header);
                 });
     }
 
-    private void addRows(PdfPTable table, matricule, grade) {
-        table.addCell("row 1, col 1");
-        table.addCell("row 1, col 2");
-        table.addCell("row 1, col 3");
-    }
-
-    private void addCustomRows(PdfPTable table)
-            throws URISyntaxException, BadElementException, IOException {
-        // Path path =
-        // Paths.get(ClassLoader.getSystemResource("Java_logo.png").toURI());
-        // Image img = Image.getInstance(path.toAbsolutePath().toString());
-        // img.scalePercent(10);
-
-        // PdfPCell imageCell = new PdfPCell(img);
-        // table.addCell(imageCell);
-
-        PdfPCell horizontalAlignCell = new PdfPCell(new Phrase("row 2, col 2"));
-        horizontalAlignCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        table.addCell(horizontalAlignCell);
-
-        PdfPCell verticalAlignCell = new PdfPCell(new Phrase("row 2, col 3"));
-        verticalAlignCell.setVerticalAlignment(Element.ALIGN_BOTTOM);
-        table.addCell(verticalAlignCell);
+    private void addRows(PdfPTable table, Paragraph data) {
+        table.addCell(data);
     }
 
     public String getFileName() {
