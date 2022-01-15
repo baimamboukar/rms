684f8674156cfd0c87ec32083a414a26d7f4f429
baimamboukar <baimamboukar@gmail.com>
2022-01-15 14:16:49 +0100

    updating pdf box

--- com/baimamboukar/java/rms/src/models/PDFBox.java
+++ com/baimamboukar/java/rms/src/models/PDFBox.java
@@ -109,17 +109,17 @@ public class PDFBox {
     }
 
     private void addTableHeader(PdfPTable table) {
-        Stream.of("column header 1", "column header 2", "column header 3")
+        Stream.of("MATRICULE", "GRADE")
                 .forEach(columnTitle -> {
                     PdfPCell header = new PdfPCell();
-                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-                    header.setBorderWidth(2);
+                    header.setBackgroundColor(BaseColor.GREEN);
+                    header.setBorderWidth(3);
                     header.setPhrase(new Phrase(columnTitle));
                     table.addCell(header);
                 });
     }
 
-    private void addRows(PdfPTable table) {
+    private void addRows(PdfPTable table, matricule, grade) {
         table.addCell("row 1, col 1");
         table.addCell("row 1, col 2");
         table.addCell("row 1, col 3");
