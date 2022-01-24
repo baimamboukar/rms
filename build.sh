7185eb62ff66713dd312ccedeacdea8f58ce41d9
baimamboukar <baimamboukar@gmail.com>
2022-01-24 01:57:54 +0100

    build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 cd com/baimamboukar/java/rms
 javac -cp ./:jars/* -d class/ src/ui/* src/database/* src/models/*
-#java -cp ./:jars/*:class/* com.baimamboukar.java.rms.src.ui.Dashboard
+ls
+java -cp ./:jars/*:class/* com.baimamboukar.java.rms.src.ui.Dashboard
