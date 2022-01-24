af6ed7fa8b7d9385be63f002ac62a4f44fce751e
baimamboukar <baimamboukar@gmail.com>
2022-01-24 02:21:39 +0100

    run

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 # javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/services/impl/*.java
 # javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/*.java
 # javac -d com/baimamboukar/java/rms/class com/baimamboukar/java/rms/database/*.java
+cd com/baimamboukar/java/rms
+jar cvfm baimamboukar.jar MANIFEST.MF class/* rms.sqlite
 
- jar cvfm baimamboukar.jar MANIFEST.MF class/* rms.sqlite
-#cd com/baimamboukar/java/rms
 #javac -cp ./:jars/* -d class/ src/ui/* src/database/* src/models/*
 #java -cp ./:rms.jar:jars/* com.baimamboukar.java.rms.src.ui.Dashboard
