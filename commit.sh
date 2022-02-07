f042279ea57fba3de3e81ef49dccfa48baebff8f
baimamboukar <baimamboukar@gmail.com>
2022-02-07 12:38:06 +0100

    commit

--- commit.sh
+++ commit.sh
@@ -1,8 +1,9 @@
- cd com/baimamboukar/java/rms/class
+ #cd com/baimamboukar/java/rms/class
+ cd javadocs
 # ls
 for file in * 
     do
             git add $file
-            git commit -m "compiling $file"
+            git commit -m "java doc $file"
             #git push
     done
